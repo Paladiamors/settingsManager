bd973f4ce8dd2533e31eca67781518c14f6cd229
Justin Ho <paladiamors@yahoo.com>
2020-08-10 01:20:32 +0900

    convert to underbar naming

--- __init__.py
+++ __init__.py
@@ -15,74 +15,75 @@ class SettingsManager:
     """
     settings manager class
     use this to setup settings
-    default location for the settings file is 
+    default location for the settings file is
     settings.json
     """
-    def __init__(self):
 
-        self.pathDict = {}
-        self.fileDict = {}
+    def __init__(self, env="dev", base_path=None):
+
+        self.path_dict = {}
+        self.file_dict = {}
         self.settings = {}
-        self.settingsFileName = "settings.json"
+        self.settings_file_name = f"settings.{env}.json"
 
         # ensure to install this package at the first level of the project
-        self.basePath = self.getBasePath()
-        self.loadSettings()
+        self.base_path = base_path or self.get_base_path()
+        self.load_settings()
 
-    def getBasePath(self):
+    def get_base_path(self):
         """
-        Return one level lower as we expect this code 
+        Return one level lower as we expect this code
         to be cloned into a project
         """
         basename = os.path.dirname(os.path.dirname(__file__))
         return basename
 
-    def loadSettings(self, settingsFileName=None):
+    def load_settings(self, settings_file_name=None):
         """
         load settings, if the settings file is located in a different location
-        then call load settings again with the name of the file to load 
+        then call load settings again with the name of the file to load
         """
-        if settingsFileName:
-            self.settingsFileName = settingsFileName
+        if settings_file_name:
+            self.settings_file_name = settings_file_name
 
-        settingsPath = os.path.join(self.basePath, self.settingsFileName)
+        settings_path = os.path.join(self.base_path, self.settings_file_name)
 
-        if os.path.exists(settingsPath):
-            with open(settingsPath) as handle:
+        if os.path.exists(settings_path):
+            with open(settings_path) as handle:
                 settings = json.load(handle)
 
-                self.parseSettings(settings)
+                self.parse_settings(settings)
 
-    def parseSettings(self, settings):
-        self.setupPaths(settings)
+    def parse_settings(self, settings):
+        self.setup_paths(settings)
         self.settings = settings["settings"]
 
-    def setupPaths(self, settings):
+    def setup_paths(self, settings):
         """
         sets up the file directories file paths
         """
 
-        for pathInfo in settings.get("createDirs", []):
-            os.makedirs(os.path.join(self.basePath, pathInfo["path"]), exist_ok=True)
-            pathKey = pathInfo.get("key") or os.path.basename(pathInfo["path"])
-            self.pathDict[pathKey] = os.path.join(self.basePath, pathInfo["path"])
+        for path_info in settings.get("createDirs", []):
+            os.makedirs(os.path.join(self.base_path, path_info["path"]), exist_ok=True)
+            path_key = path_info.get("key") or os.path.basename(path_info["path"])
+            self.path_dict[path_key] = os.path.join(self.base_path, path_info["path"])
 
-        for pathInfo in settings.get("dirs", []):
-            pathKey = pathInfo.get("key") or os.path.basename(pathInfo["path"])
-            self.pathDict[pathKey] = os.path.join(self.basePath, pathInfo["path"])
+        for path_info in settings.get("dirs", []):
+            path_key = path_info.get("key") or os.path.basename(path_info["path"])
+            self.path_dict[path_key] = os.path.join(self.base_path, path_info["path"])
 
         for fileInfo in settings.get("files", []):
             filename = os.path.basename(fileInfo["path"])
-            fileKey = fileInfo.get("key") or filename
-            self.fileDict[fileKey] = os.path.join(self.basePath, fileInfo["path"])
+            file_key = fileInfo.get("key") or filename
+            self.file_dict[file_key] = os.path.join(self.base_path, fileInfo["path"])
 
-    def getDir(self, key):
-        return self.pathDict[key]
+    def get_dir(self, key):
+        return self.path_dict[key]
 
-    def getFile(self, key):
-        return self.fileDict[key]
+    def get_file(self, key):
+        return self.file_dict[key]
 
-    def getSetting(self, key, default=None):
+    def get_setting(self, key, default=None):
         """
         takes a string of key1/key2
         and returns the value of self.settings["key1"]["key2"]
@@ -103,4 +104,11 @@ class SettingsManager:
         return value
 
 
-settingsManager = SettingsManager()
+settings = {}
+
+
+def get_settings(env="dev", base_path=None) -> SettingsManager:
+    if env not in settings:
+        settings[env] = SettingsManager(env=env, base_path=base_path)
+
+    return settings[env]
